ed7550ce0a01fcd3eacb868e06f8b7392478b9b2
FadedLocks702 <31429405+FadedLocks702@users.noreply.github.com>
2017-10-02 13:38:20 -0700

    Begun js assignment

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/*
+
+by default, the blog entries are collapsed
+when user clicks on the first 'read more' then the respective article collpases and the 'read more' link changes to 'read less'
+when user clicks on the second 'read more' then the respective article collpases and the 'read more' link changes to 'read less'
+
+*/
+
+/*BEGIN ASSIGNMENT
+
+$(document).ready(function() {
+
+	console.log($("article").height());
+
+$("article").css ()
+}
+
+*/

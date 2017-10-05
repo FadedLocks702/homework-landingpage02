17a34f4eebd399d2b6621b45f7b20928f1498c8e
FadedLocks702 <31429405+FadedLocks702@users.noreply.github.com>
2017-10-04 20:15:31 -0700

    I think I figured out the requirement
    Blog 1 now matching required rubric

--- js/script.js
+++ js/script.js
@@ -3,45 +3,62 @@
 
 
 
-$(document).ready(function(){
-	$("#blogone-hide").click(function(){
-		event.preventDefault();
+// $(document).ready(function(){
+// 	$("#blogone-hide").click(function(){
+// 		event.preventDefault();
 
-		var $this = $(this);
-		$this.toggleClass("#blogone-hide");
-		if($this.hasClass("#blogone-hide")){
-			$this.text('Read more');
+// 		var $this = $(this);
+// 		$this.toggleClass("#blogone-hide");
+// 		if($this.hasClass("#blogone-hide")){
+// 			$this.text('Read more');
 
-		} else {
-			$this.text('Read less');
-		}
+// 		} else {
+// 			$this.text('Read less');
+// 		}
 
-		$("#first-read-more-text").slideToggle();
-	});
-});
-// by default, blog two is collapsed
+// 		$("#first-read-more-text").slideToggle();
+// 	});
+// });
+// // by default, blog two is collapsed
 
-$(document).ready(function(){
-	$("#blogtwo-show").click(function(){
-		event.preventDefault();
+// $(document).ready(function(){
+// 	$("#blogtwo-show").click(function(){
+// 		event.preventDefault();
 
-		var $this = $(this);
-		$this.toggleClass("#blogtwo-show");
-		if($this.hasClass("#blogtwo-show")){
-			$this.text('Read less');
+// 		var $this = $(this);
+// 		$this.toggleClass("#blogtwo-show");
+// 		if($this.hasClass("#blogtwo-show")){
+// 			$this.text('Read less');
 
-		} else {
-			$this.text('Read more');
-		}
+// 		} else {
+// 			$this.text('Read more');
+// 		}
 
-		$("#second-read-more-text").slideToggle();
-	});
-});
+// 		$("#second-read-more-text").slideToggle();
+// 	});
+// });
+
+/*RUBRIC
+
+1.event.preventDefault() function - done
+2. $.ready() function - done
+3. select DOM elements with CSS selectors upon a user's click of the $.click() handler
+*/
+
+$("#blogone-hide").click(slideDown);
+
+function slideDown(){
+	$("#first-read-more-text").show()
+}
+
+function hideblog1text(){
+	$("#blogone-hide").hide()
+}
+
+$("#blogone-hide").click(hideblog1text);
+
+// $("#blogtwo-show").click()
 
 
-// when user clicks on the first 'read less' then the respective article collpases and the 'read more' link changes to 'read less'
-
-
-// when user clicks on the second 'read more' then the respective article collpases and the 'read less' link changes to 'read more'
 
 

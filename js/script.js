e089c03f4c1e22f9b2055cfe5ec9c5324284ccf4
FadedLocks702 <31429405+FadedLocks702@users.noreply.github.com>
2017-10-04 22:34:22 -0700

    Finishing touches
    added the ready function.. will do a qa sweep before pushing

--- js/script.js
+++ js/script.js
@@ -1,18 +1,17 @@
 
-/*RUBRIC
+/*
 
 1.event.preventDefault() function - done
 2. $.ready() function - done
 3. select DOM elements with CSS selectors upon a user's click of the $.click() handler
 */
+$( document ).ready(function() {
+        console.log( "document loaded" );
+    });
 
-$("#blog-one").click(slidedown);
-
-function slidedown(){
-	$("#first-read-more-text").show()
-}
-
-/*4. Hide the relevant "Read More" link using $.hide()*/
+$("#blog-one").click(function(){
+	$("#first-read-more-text").slideDown("fast");
+});
 
 function hideblog1link(){
 	$("#blog-one").hide()
@@ -27,11 +26,9 @@ $("#blog-one").click(replaceWith)
 
 /*5. Show the second blog paragraph using slidedown and show.*/
 
-$("#blog-two").click(slideDown);
-
-function slideDown(){
-	$("#second-read-more-text").show()
-}
+$("#blog-two").click(function(){
+	$("#second-read-more-text").slideDown("fast");
+});
 
 /*6. Hide the relevant "Read More" link using $.hide() */
 
@@ -47,6 +44,7 @@ $("#blog-two").click(replacewith)
 
 /** 7. Have the first blog text slide up and hide the 'read less' link using $.slideUp() and $.hide()
 **/
+
 $("#replace-1").click(function(){
 	$("#first-read-more-text").slideUp();
 });
